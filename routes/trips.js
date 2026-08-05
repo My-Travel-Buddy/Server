dfa7393abf6200eb8245652178f3b4061f839ea9
MathewAyala <mathewayala05@gmail.com>
2026-08-05 15:33:53 -0400

    fix: added a conditional to patch and delete

--- routes/trips.js
+++ routes/trips.js
@@ -36,9 +36,13 @@ router.post('/', async (req, res, next) => {
 
 router.patch('/:id', async (req, res, next) => {
     try{
-        const id = Number(req.params.id),
+        const id = Number(req.params.id);
         const trip = await Trip.findByPk(id)
         
+        if (!trip) {
+            return res.status(404).json({ error: 'Trip not found' });
+        }
+
         await trip.update(req.body)
         res.status(200).json(trip)
     }catch(err){
@@ -51,6 +55,9 @@ router.delete('/:id', async (req, res, next) => {
         const id = Number(req.params.id);
         const trip = await Trip.findByPk(id);
 
+        if (!trip) {
+            return res.status(404).json({ error: 'Trip not found' });
+        }
         await trip.destroy()
         res.sendStatus(410)   
     }catch(err){
