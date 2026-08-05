4cb66571c6d04fe2e8c9f155f1c2d4d697d1f025
MathewAyala <mathewayala05@gmail.com>
2026-08-05 15:12:35 -0400

    completed: trip routes

--- routes/trips.js
+++ routes/trips.js
@@ -1,4 +1,61 @@
 const { Trip } = require('../models')
 const router = require('express').Router();
 
-router.Get
+router.get('/', async (req, res, next) => {
+    try{
+        const trips = await Trip.findAll()
+        res.status(200).json(trips)
+    }catch(err){
+        next(err)
+    }
+})
+
+router.get('/:id', async (req, res, next) => {
+    try{
+        const id = Number(req.params.id)
+        const trip = await Trip.findByPk(id)
+        res.status(200).json(trip)
+    }catch(err){
+        next(err)
+    }
+})
+
+router.post('/', async (req, res, next) => {
+    try{
+        const {destination, date_Range, budget} = req.body
+        const trip = await Trip.create({
+            destination,
+            date_Range,
+            budget,
+        })
+        res.status(201).json(trip)
+    }catch(err){
+        next(err)
+    }
+})
+
+router.patch('/:id', async (req, res, next) => {
+    try{
+        const id = Number(req.params.id),
+        const trip = await Trip.findByPk(id)
+        
+        await trip.update(req.body)
+        res.status(200).json(trip)
+    }catch(err){
+        next(err)
+    }
+})
+
+router.delete('/:id', async (req, res, next) => {
+    try{
+        const id = Number(req.params.id);
+        const trip = await Trip.findByPk(id);
+
+        await trip.destroy()
+        res.sendStatus(410)   
+    }catch(err){
+        next(err)
+    }
+})
+
+module.exports = router;
