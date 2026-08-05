f6ebd92207542f6894259a485ddfd6ae3d51678b
Yomara <yomara024@outlook.com>
2026-08-05 12:11:53 -0400

    checklist model addded

---
+++ models/checklist.js
@@ -0,0 +1,22 @@
+const { DataTypes } = require('sequelize');
+const db = require('../db');
+
+// db.define(tableName, columns)
+// Sequelize adds id, createdAt and updatedAt columns for you automatically.
+const Checklist = db.define('checklist', {
+  tridId:{
+    type: DataTypes.UUID,
+    allowNull: false,
+  },
+  text: {
+    type: DataTypes.TEXT, // TEXT = longer free-form text
+    allowNull: false, // optional
+  },
+  completed: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false,
+    defaultValue: false, // a new task starts as "not done"
+  },
+});
+
+module.exports = Checklist;
