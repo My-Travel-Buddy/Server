450e9580be22ccb1d8cd9523a4d38c82accb19b3
MathewAyala <mathewayala05@gmail.com>
2026-08-06 10:05:19 -0400

    added auth to Trip routes, connected each router to the index, imported sequelize to User model, connected the routes to the db

--- routes/trips.js
+++ routes/trips.js
@@ -1,7 +1,8 @@
-const { Trip } = require('../models')
+const { Trip, Checklist, Activity } = require('../models')
 const router = require('express').Router();
+const { requireAuth } = require('../middleware/auth')
 
-router.get('/', async (req, res, next) => {
+router.get('/', requireAuth, async (req, res, next) => {
     try{
         const trips = await Trip.findAll()
         res.status(200).json(trips)
@@ -10,17 +11,20 @@ router.get('/', async (req, res, next) => {
     }
 })
 
-router.get('/:id', async (req, res, next) => {
+router.get('/:id', requireAuth,  async (req, res, next) => {
     try{
         const id = Number(req.params.id)
-        const trip = await Trip.findByPk(id)
+        const trip = await Trip.findOne(id, {
+            include: Checklist,
+            include: Activity,
+        })
         res.status(200).json(trip)
     }catch(err){
         next(err)
     }
 })
 
-router.post('/', async (req, res, next) => {
+router.post('/', requireAuth, async (req, res, next) => {
     try{
         const {destination, date_Range, budget} = req.body
         const trip = await Trip.create({
@@ -34,11 +38,15 @@ router.post('/', async (req, res, next) => {
     }
 })
 
-router.patch('/:id', async (req, res, next) => {
+router.patch('/:id', requireAuth, async (req, res, next) => {
     try{
-        const id = Number(req.params.id),
+        const id = Number(req.params.id);
         const trip = await Trip.findByPk(id)
         
+        if (!trip) {
+            return res.status(404).json({ error: 'Trip not found' });
+        }
+
         await trip.update(req.body)
         res.status(200).json(trip)
     }catch(err){
@@ -46,11 +54,14 @@ router.patch('/:id', async (req, res, next) => {
     }
 })
 
-router.delete('/:id', async (req, res, next) => {
+router.delete('/:id', requireAuth, async (req, res, next) => {
     try{
         const id = Number(req.params.id);
         const trip = await Trip.findByPk(id);
 
+        if (!trip) {
+            return res.status(404).json({ error: 'Trip not found' });
+        }
         await trip.destroy()
         res.sendStatus(410)   
     }catch(err){
